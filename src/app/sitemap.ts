fc3e65e336537851f4ec7be82e31eb33f301a7f7
aayushk18 <kumaraayush1807@gmail.com>
2026-06-22 12:09:06 +0530

    first commit

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,12 +4,33 @@ export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = "https://www.shreejeeindustries.in";
 
     return [
-        { url: baseUrl },
-        { url: `${baseUrl}/about` },
-        { url: `${baseUrl}/contact` },
-        { url: `${baseUrl}/why-choose-us` },
-        { url: `${baseUrl}/products` },
-        { url: `${baseUrl}/quote` },
-        { url: `${baseUrl}/industries` },
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/why-choose-us`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/products`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/quote`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/industries`,
+            lastModified: new Date(),
+        },
     ];
 }
